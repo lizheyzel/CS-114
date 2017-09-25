3662e4d06dd6f7f07965ec84a80f8556222620a6
Alex Martineau <alex.martineau@gmail.com>
2017-09-25 17:53:31 -0600

    update

--- assignment3/src/edu/ldsbc/assignment3a.java
+++ assignment3/src/edu/ldsbc/assignment3a.java
@@ -1,7 +1,30 @@
 package edu.ldsbc;
 
+/*
+@auth Alex Martineau, Lizheyzel Carranza
+Assignment 3a
+@date 9/25/2017
+ */
+
+import java.util.Scanner;
+
 public class assignment3a {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        double pi = Math.PI;
+
+        System.out.print("Enter Cylinder Radius: ");
+        double r = sc.nextDouble();
+
+        System.out.print("Enter Cylinder Length: ");
+        double l = sc.nextDouble();
+
+        double area = r * r * pi;
+        System.out.println("Area = " + area);
+
+        double vol = area * l;
+        System.out.println("Volume = " + vol);
 
     }
 }
